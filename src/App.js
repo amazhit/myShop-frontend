9e88cd21ee49cccb29d474859396f4ddd5050ec0
Abay Mazhit <amazhit@gmail.com>
2020-06-10 00:37:48 +0600

    myShop frontend 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import "./App.css";
+import data from "./data";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("sidebar-open");
+  };
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("sidebar-open");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button className="brand-button" onClick={openMenu}>
+              &#9776;
+            </button>
+            <Link to="/">Shop</Link>
+          </div>
+          <div className="header-links">
+            <a href="#">Cart</a>
+            <a href="#">SignIn</a>
+          </div>
+        </header>
+        <aside className="sidebar">
+          <h3>Categories</h3>
+          <button onClick={closeMenu}>x</button>
+          <ul>
+            <li>
+              <a href="#">Pants</a>
+            </li>
+            <li>
+              <a href="#">Shirts</a>
+            </li>
+          </ul>
+        </aside>
+        <main className="main">
+          <div className="content">
+            <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/" exact={true} component={HomeScreen} />
+          </div>
+        </main>
+        <footer className="footer">All rights are mine</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "../../node_modules/axios";
+import { useSelector, useDispatch } from "react-redux";
+import { listProducts } from "../actions/productActions";
+
+function HomeScreen(props) {
+  const productList = useSelector((state) => state.productList);
+  const { products, loading, error } = productList;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(listProducts());
+    return () => {
+      //
+    };
+  }, []);
+
+  return loading ? (
+    <div>Loading...</div>
+  ) : error ? (
+    <div>{error}</div>
+  ) : (
+    <div>
+      <ul className="products">
+        {products.map((product) => (
+          <li key={product.id}>
+            <div className="product">
+              <Link to={"/product/" + product.id}>
+                <img src={product.image} alt="" className="product-image" />
+              </Link>
+
+              <div className="product-name">
+                <Link to={"/product/" + product.id}>{product.name}</Link>
+              </div>
+              <div className="product-brand">{product.brand}</div>
+              <div className="product-price">${product.price}</div>
+              <div className="product-rating">
+                {product.rating} Stars ({product.reviews})
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default HomeScreen;
